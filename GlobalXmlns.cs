0efa34d040b520924e8a0413f113bf9c40e353cb
Corbin Crutchley <git@crutchcorn.dev>
2025-09-18 18:42:00 -0700

    chore: it works!

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KeyboardForward")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KeyboardForward.Pages")]
---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace KeyboardForward;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		hybridWebView.SetInvokeJavaScriptTarget(this);
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+	
+	private void OnSendMessageButtonClicked(object sender, EventArgs e)
+	{
+		hybridWebView.SendRawMessage($"Hello from C#!");
+	}
+
+	public async void UpArrow()
+	{
+		count++;
+
+		CounterBtn.Text = $"Clicked {count} times";
+	}
+	
+	public async void DownArrow()
+	{
+		count--;
+
+		CounterBtn.Text = $"Clicked {count} times";	
+	}
+}
